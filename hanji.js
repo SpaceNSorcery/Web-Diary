d8a885feeeb7d0940f6bcc3beca6493b6c1d46fd
JoJo <87620864+SpaceNSorcery@users.noreply.github.com>
2021-10-21 14:01:08 -0400

    Add files via upload

--- hanji.js
+++ hanji.js
@@ -53,13 +53,13 @@ function handleSubmit(event) {
 addRecordForm.addEventListener('submit', handleSubmit);
 
 var r_text = new Array ();
-r_text[0] = "All the leaves are brown";
-r_text[1] = "And the sky is grey";
-r_text[2] = "I've been for a walk";
-r_text[3] = "On a winter's day";
-r_text[4] = "I'd be safe and warm";
-r_text[5] = "If I was in L.A.";
-r_text[6] = "California dreaming, On such a winter's day";
+r_text[0] = "";
+r_text[1] = "";
+r_text[2] = "";
+r_text[3] = "";
+r_text[4] = "";
+r_text[5] = "";
+r_text[6] = "";
 Math.random();
 
 document.write();
